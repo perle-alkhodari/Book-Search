f5d2bc925a8a58066c542343734f05a6a80d61a0
perle-alkhodari <perle.alkhodari@gmail.com>
2025-04-09 14:37:27 +0300

    safely transfered all db creds in .env file

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const app = express();
 const port = 3000;
 const db = new pg.Client( {
     user: process.env.DB_USERNAME,
-    host: "localhost",
-    database: "Kotob",
-    password: "002468",
-    port: 5432
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 }); db.connect();
 const saltRounds = 10;
 
@@ -58,19 +58,11 @@ app.post("/register", async (req, res)=> {
     else {
         // User is new, send a verification email to check their email is valid and they own it.
         var code = sendVerificationEmail(userEmail);
-        var hashedPass = 
+        var hashedPass;
         bcrypt.hash(userPassword, saltRounds, async (err, hash)=> {
             hashedPass = hash;
         })
         res.render("verification.ejs", {code: code, email: userEmail, username: userName, password: hashedPass});
-
-        // bcrypt.hash(userPassword, saltRounds, async (err, hash)=> {
-        //     // Add user to db
-        //     await db.query(
-        //         "INSERT INTO users(username, email, password) VALUES($1, $2, $3);",
-        //         [userName, userEmail, hash]
-        //     )
-        // })
     }
 })
 
@@ -82,7 +74,7 @@ app.post("/verify-email", async (req, res)=> {
     var hashedPass = req.body.password;
 
     if (userCode == code) {
-        // They entered the right code so sign them in.
+        // They entered the right code so register them.
         var userName = req.body.username;
         var userEmail = req.body.email;
         var hash = req.body.password;
